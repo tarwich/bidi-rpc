2c82a5b31c7679d38b530e9fdfbb9cb5cc21981a
Samuel Dillow <tarwich@gmail.com>
2022-10-06 18:42:07 -0500

    Add testing to github

---
+++ wallaby.js
@@ -0,0 +1,11 @@
+module.exports = function (wallaby) {
+  return {
+    files: ['src/**/*.ts', '!src/**/*.test.ts'],
+    tests: ['src/**/*.test.ts'],
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
+    testFramework: 'jest',
+  };
+};
